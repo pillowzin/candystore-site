cc17df2d62341bcc822c14ed7c285ffc96ae9295
pillowzin <96745954+pillowzin@users.noreply.github.com>
2025-11-17 13:41:14 -0300

    Delete candystore-site directory

--- candystore-site/server.py
+++
@@ -1,9 +0,0 @@
-from livereload import Server
-server = Server()
-
-# arquivos que o livereload vai watchar
-server.watch("index.html")
-server.watch("style.css")
-
-
-server.serve(root='.', host='0.0.0.0', port=5500)
